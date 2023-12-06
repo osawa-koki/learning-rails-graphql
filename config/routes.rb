a7bc9209b979895f82175466bed30defda51fb96
osawa-koki <koki@osawa.dev>
2023-12-06 15:19:04 +0900

    GraphQLのGemの導入。

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post "/graphql", to: "graphql#execute"
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
 end
