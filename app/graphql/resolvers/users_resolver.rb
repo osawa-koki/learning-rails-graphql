13d5c21decf149b00ce6f450ba8d032a72a4fc0a
osawa-koki <koki@osawa.dev>
2023-12-08 23:00:34 +0900

    Usersリゾルバを別ファイル化した。

---
+++ app/graphql/resolvers/users_resolver.rb
@@ -0,0 +1,11 @@
+module Resolvers
+  class UsersResolver < GraphQL::Schema::Resolver
+    description 'List all users.'
+
+    type [Types::UserType], null: false
+
+    def resolve
+      User.all
+    end
+  end
+end
